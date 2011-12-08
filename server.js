087544a9cf32bbe05eaa981b7fd1eb752ca2d3d6
Chris Matthieu <chris@matthieu.us>
2011-12-08 14:14:25 -0700

    added nebula example and images

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (outerspace) is running..');
-}).listen(12885);
+// var http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World\nApp (outerspace) is running..');
+// }).listen(12885);
+
+
+var express = require("express"),
+    app = express.createServer();
+app.use(express.static(__dirname));
+app.listen(12885);
