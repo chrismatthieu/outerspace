1ef6b4023acde78df7b99cadfbad9e81cf39eb77
Chris Matthieu <chris@matthieu.us>
2011-12-08 14:18:54 -0700

    cleaned server.js

--- server.js
+++ server.js
@@ -1,10 +1,3 @@
-// var http = require('http');
-// http.createServer(function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   res.end('Hello World\nApp (outerspace) is running..');
-// }).listen(12885);
-
-
 var express = require("express"),
     app = express.createServer();
 app.use(express.static(__dirname));
